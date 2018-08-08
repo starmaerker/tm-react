c955ed62f6001dda672a8edf69e3327de8338d2c
Andreas Gropp <starmaerker@googlemail.com>
2018-08-08 08:13:45 +0200

    refactoring api.js

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,19 +1,16 @@
-var axios = require('axios');
+const axios = require('axios');
 
-var id = "YOUR_CLIENT_ID";
-var sec = "YOUR_SECRET_ID";
-var params = "?client_id=" + id + "&client_secret=" + sec;
+const id = "YOUR_CLIENT_ID";
+const sec = "YOUR_SECRET_ID";
+const params = `?client_id=${id}&client_secret=${sec}`;
 
 function getProfile(username) {
-    return axios.get('https://api.github.com/users/' + username + params)
-        .then(function(user) {
-            return user.data;
-        })
-
-}
+    return axios.get(`https://api.github.com/users/${username}${params}`)
+        .then(({ data }) => data);
+    }
 
 function getRepos(username) {
-    return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
+    return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
 }
 
 function getStarCount(repos) {
